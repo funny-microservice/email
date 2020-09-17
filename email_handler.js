565f5f29f461eb74e60da1d9f6fbdb057b437426
xiekun1992 <xkfront@outlook.com>
2020-09-18 01:08:46 +0800

    add nodemailer

--- email_handler.js
+++ email_handler.js
@@ -1,11 +1,21 @@
+'use strict'
+
 const seneca = require('seneca')()
 
 seneca
   .add('cmd:email', function(msg, respond) {
     console.log(msg.args)
-    respond(null, {
-      msg: 'success email'
+    require('./email_service')({
+      to: 'xiekun@f3dt.com',
+      subject: 'welcome',
+      html: '<h1>welcome!!</h1>'
     })
+      .then((result) => {
+        respond(null, {
+          msg: result
+        })
+      })
+      .catch(respond)
   })
   .listen({
     pin: 'cmd:email'
---
+++ email_service.js
@@ -0,0 +1,25 @@
+
+const nodemailer = require('nodemailer')
+const secret = require('./config.json')
+
+module.exports = async function send(options) {
+    const transporter = nodemailer.createTransport({
+        // host: 'smtp.outlook.com',
+        host: secret.host,
+        port: 587,
+        secure: false,
+        auth: {
+            user: secret.user,
+            pass: secret.pass
+        }
+    })
+    const info = await transporter.sendMail({
+        from: `xk email service<${secret.user}>`,
+        to: options.to,
+        subject: options.subject,
+        // text: '',
+        html: options.html
+    })
+    console.log(info)
+    return info
+}
