f4bf8107bafb44c6431d0107ad412dd07d1d918b
xiekun1992 <xkfront@outlook.com>
2020-09-20 18:49:57 +0800

    refactor files

--- src/email_service.js
+++ src/email_service.js
@@ -2,6 +2,14 @@
 const nodemailer = require('nodemailer')
 const secret = require('../config.json')
 
+function restoreSecret(str) {
+    return Buffer.from(str, 'base64').toString()
+}
+
+secret.pass = restoreSecret(secret.pass)
+secret.user = restoreSecret(secret.user)
+secret.host = restoreSecret(secret.host)
+
 module.exports = async function send(options) {
     const transporter = nodemailer.createTransport({
         // host: 'smtp.outlook.com',
